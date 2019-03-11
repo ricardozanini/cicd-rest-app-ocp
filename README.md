53c16fc95b7670351ee7e82eb399e15a6495640a
Ricardo Zanini <ricardozanini@gmail.com>
2019-03-11 18:18:02 -0300

    changing bc strategy

--- README.md
+++ README.md
@@ -17,3 +17,7 @@ This is a sample project that can be used in demos and/or as reference to build 
 ### References
 
 - [Application CI/CD on OpenShift Container Platform with Jenkins](https://access.redhat.com/documentation/en-us/reference_architectures/2017/html-single/application_cicd_on_openshift_container_platform_with_jenkins/index)
+- [OpenShift Docs - Build Inputs](https://docs.openshift.com/container-platform/3.11/dev_guide/builds/build_inputs.html#binary-source)
+- [OpenShift Docs - Docker Strategy Options](https://docs.openshift.com/container-platform/3.11/dev_guide/builds/build_strategies.html#docker-strategy-options)
+- [Enhancing your Builds on OpenShift: Chaining Builds](https://blog.openshift.com/chaining-builds/)
+- [OpenShift Docs - Binary Builds](https://docs.openshift.com/container-platform/3.11/dev_guide/dev_tutorials/binary_builds.html)
---
+++ pipelines/build-dev-promote-stg.groovy
@@ -0,0 +1,71 @@
+def appVersion = ""
+
+pipeline {
+    agent any
+    stages {
+        stage("Checkout") {
+            agent {
+                label 'maven'
+            }  
+            steps {
+                git url "${env.GIT_REPOSITORY}", branch: "${env.GIT_BRANCH}"
+            }
+        }
+        stage("Build Aplication") {
+            agent {
+                label 'maven'
+            }   
+            steps {
+                def pom = readMavenPom file: "${env.APP_NAME}/pom.xml"
+                appVersion = "${pom.version}-${env.BUILD_NUMBER}"
+                sh "mvn -B versions:set -DnewVersion=${appVersion}"
+                sh "mvn -B -Dmaven.test.skip=true clean package"
+                sh "cp target/${env.APP_NAME}-${appVersion}.jar target/app.jar"
+                stash name: "artifact", includes: "target/app.jar"
+            }
+        }
+        stage("Unit Tests") {    
+            agent {
+                label 'maven'
+            }   
+            steps {
+                sh "mvn -B clean test"
+                stash name: "unit_tests", includes: "target/surefire-reports/**"
+            }
+        }
+        stage("Integration Tests") {
+            agent {
+                label 'maven'
+            }
+            steps {
+                sh "mvn -B clean verify -Dsurefire.skip=true"
+                stash name: 'it_tests', includes: 'target/failsafe-reports/**'
+            }
+        }
+        stage("Build and Tag Image") {
+            steps {
+                script {
+                    openshift.withCluster() {
+                        openshift.withProject() {
+                            unstash name: "artifact"
+                            def buildSelector = openshift.selector("bc/${env.APP_NAME}-docker").startBuild("--from-file='target/app.jar'")
+                            //todo: throw expcetion if doesn't exist
+                            buildSelector.logs("-f")        
+                            def dc  = openshift.selector("dc", env.APP_NAME)
+                            dc.rollout().status()
+                        }
+                    }
+                }
+            }
+        }
+        stage("Approval") {
+            agent none
+            timeout(time:30, unit:'MINUTES') {
+                input 'Do I have your approval to promote this image to stage?'
+            }
+        }
+    }
+}
+
+
+
