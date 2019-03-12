329ecd0026fc3eb15daa4f26e9c6985512eb1e49
Ricardo Zanini <ricardozanini@gmail.com>
2019-03-12 18:04:43 -0300

    permissons & namespace correction

--- install.sh
+++ install.sh
@@ -9,3 +9,7 @@ oc new-project sample-rest-app-prd
 oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n sample-rest-app-dev
 oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n sample-rest-app-stg
 oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n sample-rest-app-prd
+
+# for image pulling
+oc adm policy add-cluster-role-to-user system:image-puller system:serviceaccount:cicd:jenkins
+oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:cicd:jenkins
