834d0fd3059835e47eee54a0760f79c957ed4c63
Ricardo Zanini <ricardozanini@gmail.com>
2019-03-13 16:50:35 -0300

    getting tags

--- pipelines/promote-prd.groovy
+++ pipelines/promote-prd.groovy
@@ -1,6 +1,25 @@
+def tagVersion = ""
+
 pipeline {
     agent any
     stages {
+        stage ("Grab available image versions") {
+             steps {
+                script {
+                    openshift.withCluster() {
+                        openshift.withProject("${env.PROJECT_NAME}-stg") {
+                            timeout(time:5, unit:'MINUTES') {
+                                def isTags = openshift.selector('istag', [env.APP_NAME]).objects().items()
+                                for (tag in isTags) {
+                                    echo "tag name: ${tag.name}"
+                                }
+                                
+                            }
+                        }
+                    }
+                }
+            }
+        }
         stage ("Promote to Production") {
             
         }
