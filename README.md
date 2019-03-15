0048d1957128d320b126382e748fbd7224c4158c
Ricardo Zanini <ricardozanini@gmail.com>
2019-03-15 15:32:09 -0300

    installations steps

--- README.md
+++ README.md
@@ -4,7 +4,34 @@ This is a sample project that can be used in demos and/or as reference to build 
 
 ### How to use
 
-TBD
+1. (Optional) [Deactivate the Jenkins auto-provision](https://docs.openshift.com/container-platform/3.11/install_config/configuring_pipeline_execution.html#overview) on your `master-config.yaml` file.
+
+  ```yaml
+  jenkinsPipelineConfig:
+    autoProvisionEnabled: false 
+  ```
+
+  This is needed to not have Jenkins being auto provisioned on the namespace that will run the pipelines. This way we can keep only one namespace with a single Jenkins installation. Every pipeline on the cluster will use this Jenkins.
+
+  If you're using Minishift, this file is under those three directories:
+
+  ```
+  /var/lib/minishift/base/kube-apiserver/master-config.yaml
+  /var/lib/minishift/base/openshift-apiserver/master-config.yaml
+  /var/lib/minishift/base/openshift-controller-manager/master-config.yaml
+  ```
+
+  Change all of them and restart your Minishift (or the atomic service if on a cluster).
+
+2. Install Jenkins in your cluster. Follow the directions on the OpenShift documentation by running the `jenkins-ephemeral` template.
+
+3. The `jenkins-ephemeral` won't persist your data, so attach a Volume to it pointing to `/var/home/jenkins`. 
+
+4. After cloning this repo, run the script `install.sh` logged as a `cluster-admin` (hint: `oc login -u system:admin`) to create the projects on your OpenShift cluster for this demo. It'll create three projects: `sample-rest-app-dev`, `sample-rest-app-stg` and `sample-rest-app-prd` with the proper infrastructure.
+
+5. Log on your web console, go to the `sample-rest-app-dev`, Builds, Pipelines and hit the **Start Pipeline** button. The application should be deployed on the devlopement environment. After promoting it do the same at the `sample-rest-app-stg` to have the application promoted to production. :)
+
+### Architecture
 
 ### The Sample REST Application
 
--- install.sh
+++ install.sh
@@ -1,20 +1,64 @@
 #!/bin/sh
 # TODO: turn this into an Ansilble role. :)
 
+CICD_PROJECT=$1
+SAMPLE_PROJECT="sample-rest-app"
+
+# basic validation
+validate()
+{
+    if [ -z "$CICD_PROJECT" ]; then
+        echo "Please set a name for the CI/CD project setup on your cluster. Example: ./install.sh cicd"
+        exit 1
+    fi
+}
+
 # create the applications, here we need three envs to promote the images between them
-oc new-project sample-rest-app-dev
-oc new-project sample-rest-app-stg
-oc new-project sample-rest-app-prd
+create_projects() 
+{
+    echo "oc new-project ${SAMPLE_PROJECT}-dev"
+    echo "oc new-project ${SAMPLE_PROJECT}-stg"
+    echo "oc new-project ${SAMPLE_PROJECT}-prd"
+}
 
-# let the jenkins be the admin until we figure out what roles it should have to be able to perform everything it needs.
-oc policy add-role-to-user admin system:serviceaccount:cicd:jenkins -n sample-rest-app-dev
-oc policy add-role-to-user admin system:serviceaccount:cicd:jenkins -n sample-rest-app-stg
-oc policy add-role-to-user admin system:serviceaccount:cicd:jenkins -n sample-rest-app-prd
+add_jenkins_permissions()
+{
+    # for image pulling
+    #oc adm policy add-cluster-role-to-user system:image-puller system:serviceaccount:cicd:jenkins
+    # TODO: let the jenkins be the admin until we figure out what roles it should have to be able to perform everything it needs.
+    echo "oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:${CICD_PROJECT}:jenkins"
+}
 
-# for image pulling
-oc adm policy add-cluster-role-to-user system:image-puller system:serviceaccount:cicd:jenkins
-# lazy
-oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:cicd:jenkins
+setup_application() 
+{
+    env=$1
+    pipeline=$2
+    project="${SAMPLE_PROJECT}-${env}"
 
-# trigger different deployments
-oc set triggers dc/sample-rest-app --from-image=sample-rest-app:<version> -c sample-rest-app
+    echo "oc project ${project}"
+    echo "oc process -f openshift/sample-rest-app-infrastructure.json -p NAME=\"${SAMPLE_PROJECT}\" -p JENKINS_PIPELINE=\"${pipeline}\" | oc create -f -"
+
+    if [ env = "stg" ]; then
+        echo "oc delete bc/${SAMPLE_PROJECT}-docker"
+    fi
+
+    if [ env = "prd" ]; then
+        echo "oc delete bc --all"
+    fi
+}
+
+validate
+create_projects
+add_jenkins_permissions
+
+setup_application "dev" "pipelines/build-dev-promote-stg.groovy"
+setup_application "stg" "pipelines/promote-prd.groovy"
+setup_application "prd"
+
+
+# (reference only) trigger different deployments
+#oc set triggers dc/sample-rest-app --from-image=sample-rest-app:<version> -c sample-rest-app
+
+#oc policy add-role-to-user admin system:serviceaccount:cicd:jenkins -n sample-rest-app-dev
+#oc policy add-role-to-user admin system:serviceaccount:cicd:jenkins -n sample-rest-app-stg
+#oc policy add-role-to-user admin system:serviceaccount:cicd:jenkins -n sample-rest-app-prd
